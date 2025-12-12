9e68a97a9de1d641987032a3b939b0a403e15b7d
prachi23chowdhury <prachicmpi@gmail.com>
2025-12-12 19:52:07 +0600

    added user Collection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 3000;
@@ -30,10 +30,23 @@ async function run() {
 
     const db = client.db("garments_order_db")
     const productsCollection = db.collection("products")
+    const ordersCollection = db.collection("orders");
+    const usersCollection = db.collection("users");
+
+
+// user related api
+app.post("/users", async(req, res) =>{
+  const user = req.body;
+  user.role = "user";
+  user.createdAt = new Date();
+
+  const result = await usersCollection.insertOne(user)
+  res.send(result);
+})
 
     // product api
     app.get("/products", async(req, res) =>{
-        const result = await productsCollection.find().toArray()
+        const result = await productsCollection.find().toArray();  
         res.send(result)
     })
 
@@ -42,6 +55,49 @@ async function run() {
         const result = await productsCollection.insertOne(product);
         res.send(result)
     })
+
+    // our products api
+  app.get("/our-products", async (req, res) => {
+  const ourProducts = await productsCollection
+    .find()
+    .sort({ rating: -1 })
+    .limit(6)
+    .toArray();
+
+  res.send(ourProducts);
+});
+
+//  details api
+app.get("/products/:id", async (req, res) => {
+  const id = parseInt(req.params.id); 
+  const result = await productsCollection.findOne({ id: id });
+  res.send(result);
+});
+
+// orders API
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      order.createdAt = new Date();
+      // time
+      const result = await ordersCollection.insertOne(order);
+      res.send(result);
+    });
+
+    app.get("/orders", async (req, res) => {
+      const query = {};
+      const {email} = req.query;
+      if(email){
+        query.userEmail = email;
+      }
+      const cursor = ordersCollection.find(query)
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.delete("/orders/:id", async (req, res) => {
+      const result = await ordersCollection.deleteOne({ _id: new ObjectId(req.params.id) });
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
