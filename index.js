563ed8688fe3d4564168dff57501608630e9c455
prachi23chowdhury <prachicmpi@gmail.com>
2025-12-18 14:40:37 +0600

    added user api

--- index.js
+++ index.js
@@ -80,11 +80,35 @@ async function run() {
 
 // user related api
 
-app.get("/users", verifyFBToken, async(req, res) =>{
-  const cursor = usersCollection.find();
-  const result = await cursor. toArray();
-  res.send(result)
-})
+  app.get('/users', verifyFBToken, async (req, res) => {
+            const searchText = req.query.searchText;
+            const query = {};
+
+            if (searchText) {
+                // query.displayName = {$regex: searchText, $options: 'i'}
+
+                query.$or = [
+                    { displayName: { $regex: searchText, $options: 'i' } },
+                    { email: { $regex: searchText, $options: 'i' } },
+                ]
+
+            }
+
+            const cursor = usersCollection.find(query).sort({ createdAt: -1 }).limit(5);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.get('/users/:id', async (req, res) => {
+
+        })
+
+        app.get('/users/:email/role', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ role: user?.role || 'user' })
+        })
 
 app.post("/users", async(req, res) =>{
   const user = req.body;
